586d855ec70e13a219d0dd3bca254ff673e2cde8
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-11-03 12:45:09 +0600

    deploy done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
             <div className='row'>
                 <div className="col-md-6 text-white mt-5 d-none d-lg-block ">
                    <div className=' p-5 mt-5 ms-5'>
-                   <h1 className='mt-5'>CoxsBazar</h1>
+                   <h1 className='mt-5'>CoxsBazar </h1>
                    <p >Cox's Bazar Beach: The beach in Cox's Bazar is the main attraction of the town with an unbroken length of 150 km (93 mi) also termed the "longest natural unbroken sea beach" in the world. There are several 3 stars and 5 star hotels provide the exclusive beachside area with accessories for the tourist.</p> 
                    </div>
                 </div>
